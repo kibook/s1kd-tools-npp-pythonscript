7221bda551e1eb0d9a280163b9fd1ca8f2826415
kibukj <kibuzhj@gmail.com>
2019-09-17 08:59:09 -0400

    Add Applicability check script

---
+++ s1kd-tools/Applicability/Applicability check.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+import Npp
+
+def main():
+	scriptdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        filedir = os.path.dirname(Npp.notepad.getCurrentFilename())
+
+	CREATE_NO_WINDOW=0x08000000
+
+        cwd = os.getcwd()
+        os.chdir(scriptdir + "\\bin")
+
+        args = [
+                "s1kd-appcheck.exe",
+                "-d", filedir,
+                "-n",
+                "-v"]
+
+	p = subprocess.Popen(
+		args,
+		shell=True,
+		stdin=subprocess.PIPE,
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
+                env={"XML_CATALOG_FILES": os.environ["XML_CATALOG_FILES"]},
+		creationflags=CREATE_NO_WINDOW)
+		
+	(out, err) = p.communicate(Npp.editor.getText())
+	e = p.wait()
+
+	if e == 0:
+		Npp.notepad.messageBox(err)
+        else:
+                Npp.notepad.new()
+                Npp.editor.setText(err)
+
+        os.chdir(cwd)
+
+main()
